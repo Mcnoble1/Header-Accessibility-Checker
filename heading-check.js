11bd848152471f1bb0d9bc05cb3a216d8f9d182c
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-04-14 18:27:58 +0100

    feat: updated index.html with sortHeading button, and heading-check.js with sortHeading function :octocat:

--- heading-check.js
+++ heading-check.js
@@ -1,40 +1,27 @@
 const heading = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
-console.log(heading);
 const mainArray = [];
 heading.forEach(element => {
     mainArray.push(element.localName);
 });
 console.log(mainArray);
+// const newArray = mainArray.sort();
+// console.log(newArray);
 
-const newArray = mainArray.sort();
+// const mainArray = ['h2','h3','h1','h5','h4'];
+// const newArray = ['h1','h2','h3','h4','h5'];
 
-console.log(newArray);
-
-// const e = document.getElementsByTagName('h2')[0];
-// console.log(e)
-// const d = document.createElement('h1');
-// console.log(d)
-// d.innerHTML = e.innerHTML;  
-
-// e.parentNode.replaceChild(d, e);
-
-mainArray.forEach(oldHeading => {
-    newArray.forEach(newHeading => {
-        const e = document.getElementsByTagName(oldHeading)[0];
-        const d = document.createElement(newHeading);
-        d.innerHTML = e.innerHTML;
-        e.parentNode.replaceChild(d, e);
-    });    
-    
-   });
-
-
-
-
-// function correctOrder() {
-//     var Element = document.getElementsByTagName("p");
-//     for (var i = 0; i < Element.length; i++) {
-//         alert(Element[i].innerHTML);
-//         Element[i].innerHTML = "GeeksforGeeks.";
-//     }
-// }
+function sortHeading() {
+    let i = 0;
+    while (i < mainArray.length) {
+        const e = document.getElementsByTagName(mainArray[i])[0];
+            if ( e === `h${1}` ) {
+                console.log('no change')
+            } else if ( e !== `h${1}` ) {
+                const d = document.createElement(`h${i + 1}`);
+                d.innerHTML = e.innerHTML;  
+                e.parentNode.replaceChild(d, e);
+                console.log(e)
+                console.log(d)
+                i++;
+             }
+}};
