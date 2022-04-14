c8e1c5c8ffe3bfb7919731f631e41d76a083a376
Idowu Festus <40045755+Mcnoble1@users.noreply.github.com>
2022-04-14 13:38:07 +0100

    feat: updated header checking function :octocat:

--- heading-check.js
+++ heading-check.js
@@ -1,13 +1,40 @@
-let heading = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
+const heading = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 console.log(heading);
-let headingArray = Array.from(heading);
-console.log(headingArray)
-let headingArrayLength = headingArray.length;
-console.log(headingArrayLength);
-let headingArrayIndex = 0;
-let headingArrayElement = headingArray[headingArrayIndex];
-console.log(headingArrayElement);
-let headingArrayElementText = headingArrayElement.innerText;
-console.log(headingArrayElementText);
-let headingArrayElementTextLength = headingArrayElementText.length;
-console.log(headingArrayElementTextLength);
+const mainArray = [];
+heading.forEach(element => {
+    mainArray.push(element.localName);
+});
+console.log(mainArray);
+
+const newArray = mainArray.sort();
+
+console.log(newArray);
+
+// const e = document.getElementsByTagName('h2')[0];
+// console.log(e)
+// const d = document.createElement('h1');
+// console.log(d)
+// d.innerHTML = e.innerHTML;  
+
+// e.parentNode.replaceChild(d, e);
+
+mainArray.forEach(oldHeading => {
+    newArray.forEach(newHeading => {
+        const e = document.getElementsByTagName(oldHeading)[0];
+        const d = document.createElement(newHeading);
+        d.innerHTML = e.innerHTML;
+        e.parentNode.replaceChild(d, e);
+    });    
+    
+   });
+
+
+
+
+// function correctOrder() {
+//     var Element = document.getElementsByTagName("p");
+//     for (var i = 0; i < Element.length; i++) {
+//         alert(Element[i].innerHTML);
+//         Element[i].innerHTML = "GeeksforGeeks.";
+//     }
+// }
